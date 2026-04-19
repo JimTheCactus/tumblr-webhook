76ff654a4b9d530a672013e50596986706bab925
JimTheCactus <git@jimthecactus.com>
2026-04-19 23:43:56 +0000

    Format and documentation updates

--- src/index.mjs
+++ src/index.mjs
@@ -1,13 +1,20 @@
 import Tumblr from "tumblr.js";
-import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager"
-import { DynamoDBClient, GetItemCommand, UpdateItemCommand } from "@aws-sdk/client-dynamodb"
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  UpdateItemCommand,
+} from "@aws-sdk/client-dynamodb";
 
 const MAXRETRIES = 5;
-const secretClient = new SecretsManagerClient({})
+const secretClient = new SecretsManagerClient({});
 const secretCommmand = new GetSecretValueCommand({
-  SecretId: process.env.CONFIGSECRET
-})
-const secretResult = await secretClient.send(secretCommmand)
+  SecretId: process.env.CONFIGSECRET,
+});
+const secretResult = await secretClient.send(secretCommmand);
 const config = JSON.parse(secretResult.SecretString);
 
 const dbClient = new DynamoDBClient({});
@@ -22,34 +29,34 @@ const setLastPost = async (postId) => {
   const updateLastItemCommand = new UpdateItemCommand({
     TableName: tableName,
     Key: {
-      id: { "S": "lastPost" }
+      id: { S: "lastPost" },
     },
     UpdateExpression: "SET postId = :data",
     ExpressionAttributeValues: {
-      ":data": {"N": postId },
+      ":data": { N: postId },
     },
     ReturnValues: "UPDATED_NEW",
-  })
-  
+  });
+
   await dbClient.send(updateLastItemCommand);
 };
 
 const readLastPost = async () => {
-  console.log(`Reading ${tableName}`)
+  console.log(`Reading ${tableName}`);
   const getLastItemCommand = new GetItemCommand({
     TableName: tableName,
     Key: {
-      id: { "S": "lastPost" }
-    }
-  })
+      id: { S: "lastPost" },
+    },
+  });
   try {
     const result = await dbClient.send(getLastItemCommand);
     return result.Item?.postId?.N;
   } catch (err) {
-    console.error("[ERROR] DB read call errored!", err)
+    console.error("[ERROR] DB read call errored!", err);
     return undefined;
   }
-}
+};
 
 const getInitialPost = async () => {
   const dashboard = await client.userDashboard({});
@@ -67,7 +74,7 @@ const getInitialPost = async () => {
 
 const handlePost = async (post) => {
   console.info(`[INFO] Starting handling ${post.id}`);
-  
+
   try {
     if (post.tags && post.tags.indexOf("nogummy") > -1) {
       console.info("[INFO] Gummy suppression tag found. skipping.");
@@ -98,15 +105,13 @@ const handlePost = async (post) => {
     let posted = false;
     while (!posted && count < MAXRETRIES) {
       count++;
-      const response = await fetch(
-        url,
-        {
-          method: "POST",
-          headers: {
-            'Content-Type': 'application/json', // Set the content type
-          },
-          body: bodyText,
-        });
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json", // Set the content type
+        },
+        body: bodyText,
+      });
       if (response.status == 429) {
         // If we're being rate limited, wait the requested time and re-raise the event.
         const retry_after = Number(response.headers.get("Retry-After")) || 2;
@@ -114,11 +119,14 @@ const handlePost = async (post) => {
           "[WARNING] Rate Limited! Holding off " + retry_after + " s",
         );
         await Promise((resolve) => setTimeout(resolve, retry_after * 1000));
-      } else if(response.status >= 200 && response.status < 300) {
+      } else if (response.status >= 200 && response.status < 300) {
         posted = true;
       } else {
-        console.info(`[INFO] Got status ${response.status} from the server. Body: `, await response.text())
-        throw new Error("Failed to broadcast post due to server error!")
+        console.info(
+          `[INFO] Got status ${response.status} from the server. Body: `,
+          await response.text(),
+        );
+        throw new Error("Failed to broadcast post due to server error!");
       }
     }
   } catch (err) {
@@ -130,7 +138,7 @@ export const handler = async () => {
   let lastPost = await readLastPost();
 
   if (!lastPost) {
-    lastPost = await getInitialPost()
+    lastPost = await getInitialPost();
     console.info(`[INFO] Intializing DB with current post: ${lastPost}`);
     const count = await setLastPost(lastPost);
     console.info(`[INFO] Now at ${lastPost} after updating ${count} records.`);
